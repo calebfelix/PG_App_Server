b1818b59508696fff5a12c274a28a235a4b444b0
caleb <calebfelix38@gmail.com>
2021-09-20 19:07:07 +0530

    frontend backend test

--- app.js
+++ app.js
@@ -5,28 +5,35 @@ require('dotenv/config');
 const User = require('./models/User');
 const cors = require('cors');
 
+
 const app = express();
+app.set('view engine', 'ejs');
 port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, () => {console.log('DB connected...')})
 
 
 // middlewares
+app.use(express.static(__dirname + '/public'));
 app.use(cors({origin: '*',methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
+app.get('/', (req, res) => { 
+    res.render('index');
+})
 
 
-app.get('/', (req, res) => {
+
+app.get('/api', (req, res) => {
     res.json({
         version: "1.0.0",
         message: "Welcome to the PG api"
       });
 })
 
-app.get('/users', async (req, res) => {
+app.get('/api/users', async (req, res) => {
     try{        
         const users = await User.find();
         res.json(users);
@@ -35,7 +42,7 @@ app.get('/users', async (req, res) => {
     }
 })
 
-app.post('/addusr', async (req, res) => {
+app.post('/api/addusr', async (req, res) => {
     const user = new User({
         full_name: req.body.full_name,
         email: req.body.email,
