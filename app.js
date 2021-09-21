3a8692b9f8742aca46fe52a1389a1f80bee8d407
caleb <calebfelix38@gmail.com>
2021-09-21 23:32:46 +0530

    bcrypt added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const bodyParser = require("body-parser");
 require('dotenv/config');
 const User = require('./models/User');
@@ -20,18 +21,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
+// WEBPAGE
+
 app.get('/', (req, res) => { 
     res.render('Signup');
 })
 
+app.post('/signup', (req, res) => { 
+    console.log(req.body);
+    res.redirect('/');
+})
 
+app.get('/home', (req, res) => { 
+    res.render('homepage');
+})
+
+app.get('/index', (req, res) => { 
+    res.render('index');
+})
+
+app.get('/content', (req, res) => { 
+    res.render('content');
+})
+
+
+// API
 
 app.get('/api', (req, res) => {
     res.json({
         version: "1.0.0",
         message: "Welcome to the PG api"
       });
-})
+});
 
 app.get('/api/users', async (req, res) => {
     try{        
@@ -40,24 +61,51 @@ app.get('/api/users', async (req, res) => {
     }catch(error){
         res.json(error);
     }
-})
+});
 
 app.post('/api/addusr', async (req, res) => {
+    // check email exists
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.send("email already Exists")
+
+    // hashing password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    console.log(hashedPassword);
+
+    // create new user
+
     const user = new User({
         full_name: req.body.full_name,
         email: req.body.email,
         mobile: req.body.mobile,
         gender: req.body.gender,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try{
         const savedUser = await user.save();
-        res.send(savedUser)
+        res.send(`user inserted: ${savedUser._id}`)
     }catch(err){
         res.status(400).send(err);
     }
-})
+});
+
+app.post('/api/login', async (req, res) => {
+
+    // check email exists
+    console.log(req.body)
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.send("Email or Password is incorrect!!")
+
+    // check pass is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.send("Email or Password is incorrect!!");
+    
+    // if all clear
+    res.redirect('/home')
+});
 
 process.on('SIGINT', () => {
     mongoose.connection.close(() => {
