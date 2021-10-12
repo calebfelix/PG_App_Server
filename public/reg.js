5d9542d297a66d32beb52ca5d9cd5568d618b638
caleb <calebfelix38@gmail.com>
2021-10-12 20:29:43 +0530

    changes to firebase upload made

--- public/reg.js
+++ public/reg.js
@@ -69,10 +69,11 @@ document.getElementById('loader2').style.visibility = 'visible';
         console.log(myfile)
         
         const ref = firebase.storage().ref()
+        const datenow = String(Date.now());
         for (let i = 0; i < myfile.length; i++) {
             
             const file = document.querySelector("#img-files").files[i];
-            const name = `images/${usrid}/${String(Date.now())}${Owner_Name}/${String(Date.now())}_${i+1}`
+            const name = `images/${usrid}/${datenow}${Owner_Name}/${String(Date.now())}_${i+1}`
             const metadata = {
                     contentType : file.type
                 }
